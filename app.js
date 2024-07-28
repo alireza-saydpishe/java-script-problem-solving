66f9b1caa945eb05bdb471b26b4fbcf0cff40d2c
saydpishe <alirezasaydpishe20@gmail.com>
2024-07-28 15:59:24 +0330

    Solving some simple and elementary problems using javascript programming language

---
+++ app.js
@@ -0,0 +1,84 @@
+// با استفاده از جاوا اسکریپت برنامه ای بنویسد که مشخص کند اعداد وروردی فرد هستند یا زوج؟
+
+
+// var number = 15 
+// if (number % 2 == 0){
+//     console.log('even');
+// }
+
+// else{
+//     console.log('odd');
+// }
+
+
+// با استفاده از زبان جاوا اسکریپت بر نامه ای بنویسد که مشخص کننده ی گروه سنی شما باشد؟؟
+
+// var age = 1000
+// if (age > 0 && age <= 120){
+//     if (age <= 12){
+//         console.log("childe")
+//     }
+    
+//     else if(age > 12 && age <= 18){
+//         console.log("teenager")
+//     }
+    
+//     else if(age > 18 && age <= 40){
+//         console.log("young")
+//     }
+    
+//     else{
+//         console.log("oldman")
+//     }
+// }
+
+// else{
+//     console.log("your age is notvalid")
+// }
+
+// با استفاده از سوویج کیس در زبان برنامه نویسی جاوا اسکریپت برنامه ای را طراحیکنید که با گرفتن اعاد یک تا هفت روز هفته را مشخص کند؟؟
+
+// var day =5
+
+// switch (day) {
+//     case 1:
+//         console.log("شنبه")
+//         break;
+
+//     case 2:
+//         console.log("یک شنبه")
+//         break;
+
+//     case 3:
+//         console.log("دو شنبه")
+//         break;
+
+//     case 4:
+//         console.log("سه شنبه")
+//         break;
+
+//     case 5:
+//         console.log("چهار شنبه")
+//         break;
+    
+//     case 6:
+//         console.log("پنج شنبه")
+//         break;
+
+//     case 7:
+//         console.log("جمعه")
+//         break;
+
+//     default:
+//         console.log('عدد ورودی نا معتبر میباشد')
+//         break;
+// }
+
+// با استفاده از حلقه وایل  در زبان جاوا اسکریپت برنامه ای را طراحی کنید که از عدد یک تا صد را در خروجی نمایش دهد
+
+// var counter = 1
+
+// while(counter <= 100){
+//     console.log(counter)
+//     counter++
+// };
